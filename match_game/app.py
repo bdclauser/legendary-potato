69f06c772e9ee12d05f6dc18eaa73ada64f36d97
bdclauser <pzljug@hotmail.com>
2020-05-30 01:16:24 -0400

    add matched image

--- match_game/app.py
+++ match_game/app.py
@@ -9,6 +9,10 @@ display.set_caption('My Game')
 
 screen = display.set_mode((512, 512))
 
+matched = image.load('other_assets/matched.png')
+screen.blit(matched, (0, 0))
+display.flip()
+
 running = True
 
 while running:
