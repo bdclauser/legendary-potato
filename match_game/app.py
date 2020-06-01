a9a87c0e9f5b189501ab369c3f99d4e64d2894b0
bdclauser <pzljug@hotmail.com>
2020-06-01 01:18:22 -0400

    update game event

--- match_game/app.py
+++ match_game/app.py
@@ -4,6 +4,8 @@ from pygame import display, event, image
 import game_config as gc
 from animal import Animal
 
+from time import sleep
+
 
 def find_index(x, y):
     row = y // gc.IMAGE_SIZE
@@ -36,25 +38,38 @@ while running:
         if e.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             index = find_index(mouse_x, mouse_y)
-            current_images.append(index)
+            if index not in current_images:
+                current_images.append(index)
             if len(current_images) > 2:
                 current_images = current_images[1:]
 
     screen.fill((255, 255, 255))
+
+    total_skipped = 0
 
     for _, tile in enumerate(tiles):
         image_i = tile.image if tile.index in current_images else tile.box
         if not tile.skip:
             screen.blit(image_i, (tile.col * gc.IMAGE_SIZE + gc.MARGIN,
                                   tile.row * gc.IMAGE_SIZE + gc.MARGIN))
+        else:
+            total_skipped += 1
+
+    display.flip()
 
     if len(current_images) == 2:
         idx1, idx2 = current_images
         if tiles[idx1].name == tiles[idx2].name:
             tiles[idx1].skip = True
             tiles[idx2].skip = True
+            sleep(0.4)
+            screen.blit(matched, (0, 0))
+            display.flip()
+            sleep(0.4)
+            current_images = []
 
-    display.flip()
+    if total_skipped == len(tiles):
+        running = False
 
 
 print('Goodbye!')
